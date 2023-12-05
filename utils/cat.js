b854a58dfb79bcfec3bd31a43528145c32886545
gaius-98 <2388838628@qq.com>
2023-12-05 09:43:34 +0800

    fix：Sequelize 模块引入修改为自动引入注册

--- utils/cat.js
+++ utils/cat.js
@@ -1,8 +1,25 @@
 
 const env = require('../env.json')
 const { Sequelize } = require('sequelize')
-const createForm = require('../models/Form')
-const createPage = require('../models/Page')
+const path = require('path')
+const fs = require('fs')
+const modelInfo = new Map()
+const directoryPath = path.join(__dirname,'../models');  
+
+function requireFilesRecursively(directoryPath,parentPath) {  
+  const files = fs.readdirSync(directoryPath);  
+  for (const file of files) {  
+    const filePath = path.join(directoryPath, file);  
+    if (fs.lstatSync(filePath).isDirectory()) {  
+      requireFilesRecursively(filePath,  file) ; // 递归处理子文件夹  
+    } else if (path.extname(file) === '.js') {  
+      const module = require(filePath); // 引入当前文件夹下的JavaScript文件  
+      modelInfo.set(file.split('.')[0],module)
+    }  
+  }  
+}  
+requireFilesRecursively(directoryPath)
+
 const sequelize = new Sequelize({
     username:env.sql.user,
     host:env.sql.host,
@@ -10,10 +27,11 @@ const sequelize = new Sequelize({
     database:env.sql.database,  
     dialect:'mysql'
 })
-const Form = createForm(sequelize)
-const Page = createPage(sequelize)
-const cat = {
-    Form,
-    Page
+const catInfo = {}
+const autoRegisterModel = (name,createModel) =>{
+    catInfo[name] = createModel(sequelize)
 }
-module.exports = cat
+modelInfo.forEach((createFn,name)=>{
+    autoRegisterModel(name,createFn)
+})
+module.exports = catInfo
